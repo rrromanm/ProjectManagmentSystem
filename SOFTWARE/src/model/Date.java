5c5038e2a1405f7de9bfb0e1a4ee97e9bbbebd3a
kacengas <kacengas@gmail.com>
2023-11-23 20:03:28 +0100

    Copy method/Formatting Date

--- SOFTWARE/src/model/Date.java
+++ SOFTWARE/src/model/Date.java
@@ -14,25 +14,32 @@ public class Date
     int currentMonth = currentDate.getMonthValue();
     int currentYear = currentDate.getYear();
   }
+
   public Date(int day, int month, int year){
     this.day = day;
     this.month = month;
     this.year = year;
   }
+
   public void setDate(int d, int m, int y){
     day = d;
     month = m;
     year = y;
   }
-  
+
+  public Date copy()
+  {
+    return new Date(day,month,year);
+  }
+
   public void getMyDate(){
-    System.out.println("Project start date is " + day + "/" + month + "/" + year);
+    System.out.println("Project start date is: " + day + "/" + month + "/" + year);
   }
 
   public String toString(){
-    return "Day = " + day + "\n"+
-        "Month = " + month + "\n"+
-        "Year = " + year;
+    return "Day: " + day + "\n"+
+        "Month: " + month + "\n"+
+        "Year: " + year;
   }
 
   public boolean equals(Object obj)
