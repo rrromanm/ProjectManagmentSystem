0c6d5080c69fca73db8f03aa29cf6d036e13c33c
Romans Mihalonoks <roma.mihalonok@gmail.com>
2023-12-01 16:12:59 +0100

    button commercial working

--- back-end/src/view/AddCommercialViewController.java
+++ back-end/src/view/AddCommercialViewController.java
@@ -2,12 +2,11 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import model.CommercialProject;
-import model.Customer;
-import model.MyDate;
-import model.Resources;
+import main.LoadInitialData;
+import model.*;
 
 import java.awt.*;
 import java.net.URL;
@@ -76,32 +75,53 @@ public class AddCommercialViewController implements Initializable {
 
   private Button clear;
   private Button addProject;
-  @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
-    // Initialization code goes here
-  }
-  public void setAddProject(){
-    int budget = Integer.parseInt(budgetTextField.getText());
+  private ViewHandler viewHandler;
 
+  private Scene scene;
+  private ProjectModelManager projectManager;
+
+  public void init(ViewHandler viewHandler, Scene scene, ProjectModelManager projectManager)
+  {
+    this.viewHandler = viewHandler;
+    this.scene = scene;
+    this.projectManager = projectManager;
+  }
+
+  public void reset()
+  {
+
+  }
+
+  public Scene getScene()
+  {
+    return scene;
+  }
+  @FXML
+  public void setAddProject() {
+    int budget = Integer.parseInt(budgetTextField.getText());
     int day = Integer.parseInt(dayTextField.getText());
     int month = Integer.parseInt(monthTextField.getText());
     int year = Integer.parseInt(yearTextField.getText());
-
     String status = "";
-
     int projectID = Integer.parseInt(projectIDTextField.getText());
-
     int timeline = Integer.parseInt(timelineTextField.getText());
-
-    Customer customer = new Customer(firstNameTextField.getText(), surnameTextField.getText(), Integer.parseInt(customerIDTextField.getText()),
-        Integer.parseInt(phoneNumberTextField.getText()), emailTextField.getText(), addressTextField.getText());
-
-    Resources resources = new Resources(Integer.parseInt(expectedManHoursTextField.getText()), Integer.parseInt(materialExpensesTextField.getText()));
-
+    Customer customer = new Customer(firstNameTextField.getText(), surnameTextField.getText(),
+        Integer.parseInt(customerIDTextField.getText()), Integer.parseInt(phoneNumberTextField.getText()),
+        emailTextField.getText(), addressTextField.getText());
+    Resources resources = new Resources(Integer.parseInt(expectedManHoursTextField.getText()),
+        Integer.parseInt(materialExpensesTextField.getText()));
     int size = Integer.parseInt(sizeTextField.getText());
     short floors = Short.parseShort(floorsTextField.getText());
-    commercialProject = new CommercialProject(budget, new MyDate(day, month, year), status, projectID, timeline, customer, resources, size, floors, usageTextField.getText());
+    commercialProject = new CommercialProject(budget, new MyDate(day, month, year), status, projectID, timeline,
+        customer, resources, size, floors, usageTextField.getText());
+    ProjectList projectList = new ProjectList();
+    projectList.addProject(commercialProject);
+
+    reset();
   }
 
+  @Override public void initialize(URL location, ResourceBundle resources)
+  {
 
+  }
 }
