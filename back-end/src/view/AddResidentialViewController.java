9b2d8c2124d6cd90622c9e3204f640e5fc7cf96d
kacengas <kacengas@gmail.com>
2023-12-04 14:06:11 +0100

    Road/Residential GUI improvements

--- back-end/src/view/AddResidentialViewController.java
+++ back-end/src/view/AddResidentialViewController.java
@@ -6,7 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import model.ProjectModelManager;
+import model.*;
+
+import javax.swing.*;
 
 public class AddResidentialViewController
 {
@@ -18,14 +20,12 @@ public class AddResidentialViewController
   @FXML private TextField monthField;
   @FXML private TextField yearField;
   @FXML private ComboBox statusComboBox;
+  @FXML private ComboBox stateComboBox;
   @FXML private TextField projectIDField;
   @FXML private TextField timelineField;
   @FXML private TextField firstNameField;
   @FXML private TextField surnameField;
   @FXML private TextField customerIDField;
-  @FXML private TextField phoneNumberField;
-  @FXML private TextField emailField;
-  @FXML private TextField addressField;
   @FXML private TextField expectedManHoursField;
   @FXML private TextField materialExpensesField;
   @FXML private TextField sizeField;
@@ -35,17 +35,39 @@ public class AddResidentialViewController
   @FXML private Button clearButton;
   @FXML private Button addProjectButton;
   @FXML private Button backButton;
+  @FXML private TextField projectTypeField;
 
   public void init(ViewHandler viewHandler, Scene scene, ProjectModelManager projectManager)
   {
     this.viewHandler = viewHandler;
     this.scene = scene;
     this.projectManager = projectManager;
+    String[] statusString = {"Under Construction","Finished","Planned"};
+    statusComboBox.getItems().addAll(statusString);
+    statusComboBox.getSelectionModel().selectFirst();
+    String[] stateString = {"New Build","Reconstruction"};
+    stateComboBox.getItems().addAll(stateString);
+    statusComboBox.getSelectionModel().selectFirst();
   }
 
   public void reset()
   {
-
+    budgetField.setText("");
+    dayField.setText("");
+    monthField.setText("");
+    yearField.setText("");
+    statusComboBox.getSelectionModel().selectFirst();
+    projectIDField.setText("");
+    timelineField.setText("9");
+    firstNameField.setText("");
+    surnameField.setText("");
+    customerIDField.setText("");
+    expectedManHoursField.setText("");
+    materialExpensesField.setText("");
+    sizeField.setText("");
+    NrOfKitchens.setText("1");
+    NrOfBathrooms.setText("1");
+    RoomsWithPlumbing.setText("2");
   }
 
   public Scene getScene()
@@ -53,15 +75,207 @@ public class AddResidentialViewController
     return scene;
   }
 
+  public boolean isValidString(String string)
+  {
+    return string.matches("[a-zA-Z]+");
+  }
+
   public void handleActions(ActionEvent e)
   {
     if (e.getSource() == clearButton)
     {
-
+      reset();
     }
     else if (e.getSource() == backButton)
     {
       viewHandler.openView("AddProjectView");
+    }
+    else if(e.getSource() == addProjectButton)
+    {
+      ProjectList projects = new ProjectList();
+      ProjectModelManager manager = new ProjectModelManager("projects.bin");
+
+      int budget = 0;
+      int day = 0;
+      int month = 0;
+      int year = 0;
+      String status = null;
+      int projectID = 0;
+      int timeline = 0;
+      String firstName = null;
+      String surname = null;
+      int customerID = 0;
+      int expectedManHours = 0;
+      int materialExpenses = 0;
+      int size = 0;
+      int kitchens = 0;
+      int bathrooms = 0;
+      int plumbing = 0;
+      String state = null;
+
+      String type = projectTypeField.getText();
+      try
+      {
+        budget = Integer.parseInt(budgetField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect budget inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        day = Integer.parseInt(dayField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect day inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        month = Integer.parseInt(monthField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect month inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        year = Integer.parseInt(yearField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect year inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      status = (String) statusComboBox.getValue();
+      try
+      {
+        projectID = Integer.parseInt(projectIDField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect project ID inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        timeline = Integer.parseInt(timelineField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect timeline inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(firstNameField.getText()))
+      {
+        firstName = firstNameField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect customer first name inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(surnameField.getText()))
+      {
+        surname = surnameField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect customer surname inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        customerID = Integer.parseInt(customerIDField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect customer ID inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        expectedManHours = Integer.parseInt(expectedManHoursField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect expected man hours inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        materialExpenses = Integer.parseInt(materialExpensesField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect material expenses inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        size = Integer.parseInt(sizeField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect size of the facility inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        bathrooms = Integer.parseInt(NrOfBathrooms.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect number inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        kitchens = Integer.parseInt(NrOfKitchens.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect number inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        plumbing = Integer.parseInt(RoomsWithPlumbing.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect number inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      state = (String) stateComboBox.getValue();
+
+
+      MyDate date = new MyDate(day, month, year);
+      Customer customer = new Customer(firstName, surname, customerID);
+      Resources resources = new Resources(expectedManHours, materialExpenses);
+      projects.addProject(
+          new ResidentialProjects(budget, date, status, projectID, timeline,
+              customer, resources, size, kitchens, bathrooms, plumbing, state));
+      manager.appendProjects(projects);
+      reset();
     }
   }
 }
