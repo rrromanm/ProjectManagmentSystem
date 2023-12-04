90f156d1de3530687265f8e91362f5846e2922e8
Richardzik <kubrama04@gmail.com>
2023-12-04 12:12:44 +0100

    Industrial Adding project (done)

--- back-end/src/view/AddIndustrialViewController.java
+++ back-end/src/view/AddIndustrialViewController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import model.ProjectModelManager;
+import model.*;
+
+import javax.swing.*;
 
 public class AddIndustrialViewController
 {
@@ -43,11 +46,35 @@ public class AddIndustrialViewController
     this.viewHandler = viewHandler;
     this.scene = scene;
     this.projectManager = projectManager;
+    String[] statusString = {"Under Construction","Finished","Planned"};
+    statusComboBox.getItems().addAll(statusString);
+    statusComboBox.getSelectionModel().selectFirst();
   }
+  public boolean isValidString(String string)
+  {
+    return string.matches("[a-zA-Z]+");
+  }
+
 
   public void reset()
   {
-
+    budgetField.setText("");
+    dayField.setText("");
+    monthField.setText("");
+    yearField.setText("");
+    statusComboBox.getSelectionModel().selectFirst();
+    projectIDField.setText("");
+    timelineField.setText("");
+    firstNameField.setText("");
+    surnameField.setText("");
+    customerIDField.setText("");
+    phoneNumberField.setText("");
+    emailField.setText("");
+    addressField.setText("");
+    expectedManHoursField.setText("");
+    materialExpensesField.setText("");
+    sizeField.setText("");
+    typeOfTheFacilityField.setText("");
   }
 
   public Scene getScene()
@@ -61,6 +88,205 @@ public class AddIndustrialViewController
     {
       viewHandler.openView("AddProjectView");
     }
-    
+    else if(e.getSource() == clearButton)
+    {
+      reset();
+    }
+    else if(e.getSource() == addProjectButton)
+    {
+      ProjectList projects = new ProjectList();
+      ProjectModelManager manager = new ProjectModelManager("projects.bin");
+
+      int budget = 0;
+      int day = 0;
+      int month = 0;
+      int year = 0;
+      String status = null;
+      int projectID = 0;
+      int timeline = 0;
+      String firstName = null;
+      String surname = null;
+      int customerID = 0;
+      int phoneNumber = 0;
+      String email = null;
+      String address = null;
+      int expectedManHours = 0;
+      int materialExpenses = 0;
+      int size = 0;
+      String typeOfFacility = null;
+
+      String type = projectTypeField.getText();
+      try
+      {
+        budget = Integer.parseInt(budgetField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect budget inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        day = Integer.parseInt(dayField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect day inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        month = Integer.parseInt(monthField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect month inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        year = Integer.parseInt(yearField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect year inputted", "ERROR",
+            JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      status = (String) statusComboBox.getValue();
+      try
+      {
+        projectID = Integer.parseInt(projectIDField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect project ID inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        timeline = Integer.parseInt(timelineField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect timeline inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(firstNameField.getText()))
+      {
+        firstName = firstNameField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect customer first name inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(surnameField.getText()))
+      {
+        surname = surnameField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect customer surname inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        customerID = Integer.parseInt(customerIDField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect customer ID inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        phoneNumber = Integer.parseInt(phoneNumberField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect phone number inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(emailField.getText()))
+      {
+        email = emailField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null, "Incorrect customer email inputted",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(addressField.getText()))
+      {
+        address = addressField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect customer address inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        expectedManHours = Integer.parseInt(expectedManHoursField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect expected man hours inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        materialExpenses = Integer.parseInt(materialExpensesField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect material expenses inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      try
+      {
+        size = Integer.parseInt(sizeField.getText());
+      }
+      catch (NumberFormatException exception)
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect size of the facility inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      if (isValidString(typeOfTheFacilityField.getText()))
+      {
+        typeOfFacility = typeOfTheFacilityField.getText();
+      }
+      else
+      {
+        JOptionPane.showMessageDialog(null,
+            "Incorrect type of the facility inputted", "ERROR", JOptionPane.ERROR_MESSAGE);
+        return;
+      }
+      MyDate date = new MyDate(day, month, year);
+      Customer customer = new Customer(firstName, surname, customerID,
+          phoneNumber, email, address);
+      Resources resources = new Resources(expectedManHours, materialExpenses);
+      projects.addProject(
+          new IndustrialProjects(budget, date, status, projectID, timeline,
+              customer, resources, size, typeOfFacility));
+
+      manager.appendProjects(projects);
+      reset();
+    }
   }
 }
