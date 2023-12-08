9e25c0c69ee12131b0ecfa4be02537d7345dc30e
Romans Mihalonoks <roma.mihalonok@gmail.com>
2023-12-08 15:29:21 +0100

    website date fixed

--- front-end/SEP1-WEB FINAL/js/script.js
+++ front-end/SEP1-WEB FINAL/js/script.js
@@ -7,12 +7,24 @@ $(document).ready(function () {
         success: function (xml) {
             // Parse XML and populate table
             $(xml).find('projects').each(function () { // Adjusted selector to 'projects'
+                var name = $(this).find('name').text();
                 var budget = $(this).find('budget').text();
-                var startTime = $(this).find('startTime').text();
-                var endTime = $(this).find('endTime').text();
+
+                var startTime = $(this).find('startTime');
+                var startDay = startTime.find('day').text();
+                var startMonth = startTime.find('month').text();
+                var startYear = startTime.find('year').text();
+                startTime = startDay + '/' + startMonth + '/' + startYear;
+
+                var endTime = $(this).find('endTime');
+                var endDay = endTime.find('day').text();
+                var endMonth = endTime.find('month').text();
+                var endYear = endTime.find('year').text();
+                endTime = endDay + '/' + endMonth + '/' + endYear;
+
                 var customer = $(this).find('customer').find('firstName').text(); // Updated to find 'firstName' within 'customer'
-                var resources = $(this).find('resources').find('manHoursUsed').text();
-                var expenses = $(this).find('resources').find('materialExpenses').text();
+                var manHoursUsed = $(this).find('resources').find('manHoursUsed').text();
+                var expectedManHours = $(this).find('resources').find('expectedManHours').text();
                 var status = $(this).find('status').text();
                 var projectID = $(this).find('projectID').text();
                 var timeline = $(this).find('timeline').text();
@@ -20,11 +32,12 @@ $(document).ready(function () {
 
                 // Append data to the table
                 $('#xmlData').append('<tr>' +
+                    '<td>' + name + '</td>' +
                     '<td>' + projectID + '</td>' +
                     '<td>' + customer + '</td>' +
                     '<td>' + startTime + '</td>' +
                     '<td>' + endTime + '</td>' +
-                    '<td>' + resources + "/" + expenses + '</td>' +
+                    '<td>' + manHoursUsed + " / " + expectedManHours + '</td>' +
                     '<td>' + status + '</td>' +
                     '<td>' + budget + '</td>' +
                     '<td>' + timeline + '</td>' +
